cac1925ed3e899cc310425203fb071efdfd4f243
Luca Venturelli <venturel@users.noreply.github.com>
2017-02-02 18:39:32 +0100

    added new module adding notification option (#5)

---
+++ deployment-group-ec2tag-notify/main.tf
@@ -0,0 +1,17 @@
+resource "aws_codedeploy_deployment_group" "deployment_group" {
+  app_name              = "${var.app_name}"
+  deployment_group_name = "${var.app_name}-${var.environment}"
+  service_role_arn      = "${var.service_role_arn}"
+
+  trigger_configuration {
+      trigger_events = "${var.trigger_events}"
+      trigger_name = "${var.trigger_name}"
+      trigger_target_arn = "${var.trigger_arn}"
+  }
+
+  ec2_tag_filter {
+    key   = "${var.filterkey}"
+    type  = "KEY_AND_VALUE"
+    value = "${var.filtervalue}"
+  }
+}
