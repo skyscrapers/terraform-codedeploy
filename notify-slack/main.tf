f89f19e73bcfb37fe8fa7bf4cdde24f079a9858d
Luca <luca@skyscrapers.eu>
2017-06-06 17:17:18 +0200

    adding notifications to codedeploy and lambda function

---
+++ notify-slack/main.tf
@@ -0,0 +1,74 @@
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "default_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "cd_sns_lambda_policy" {
+  name = "cd_sns-lambda-policy"
+  role = "${aws_iam_role.iam_for_lambda.id}"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [{
+    "Effect": "Allow",
+    "Action": [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "kms:Decrypt"
+    ],
+    "Resource": "*"
+  }]
+}
+EOF
+}
+
+resource "aws_sns_topic" "cd-sns-lambda" {
+    name = "cd-sns-lambda"
+}
+
+resource "aws_sns_topic_subscription" "lambda-subscription" {
+    topic_arn = "${aws_sns_topic.cd-sns-lambda.arn}"
+    protocol = "lambda"
+    endpoint = "${aws_lambda_function.cd_sns_lambda.arn}"
+}
+
+resource "aws_lambda_function" "cd_sns_lambda" {
+  function_name     = "cd_sns_lambda"
+  role              = "${aws_iam_role.iam_for_lambda.arn}"
+  handler           = "lambda-slack.lambda_handler"
+  s3_bucket         = "${var.lambda_bucket_name}"
+  s3_key            = "lamdba-slack.zip"
+  s3_object_version = "${var.lambda_bucket_version}"
+  runtime           = "python2.7"
+  timeout           = "120"
+  kms_key_arn       = "${var.kms_key_arn}"
+  environment {
+    variables = {
+      SLACK_WEBHOOK = "${var.slack_webhook_url}"
+      SLACK_CHANNEL = "${var.slack_channel}"
+    }
+  }
+}
+
+resource "aws_lambda_permission" "cd_sns_lambda" {
+  statement_id   = "AllowExecutionFromCloudWatch"
+  action         = "lambda:InvokeFunction"
+  function_name  = "${aws_lambda_function.cd_sns_lambda.function_name}"
+  principal      = "events.amazonaws.com"
+  source_arn     = "${aws_sns_topic_subscription.lambda-subscription.arn}"
+}
