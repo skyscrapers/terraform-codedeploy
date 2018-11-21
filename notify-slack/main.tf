79a170fd4cba0b475e83bd738cb8732e16b24fa8
Adey Merrett <Komorebi-E@users.noreply.github.com>
2018-11-21 14:36:45 +0100

    tf fmt

--- notify-slack/main.tf
+++ notify-slack/main.tf
@@ -20,6 +20,7 @@ EOF
 resource "aws_iam_role_policy" "cd_sns_lambda_policy" {
   name = "cd_sns-lambda-policy"
   role = "${aws_iam_role.iam_for_lambda.id}"
+
   policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -38,55 +39,56 @@ EOF
 }
 
 resource "aws_sns_topic" "cd-sns-lambda" {
-    name = "cd-sns-lambda"
+  name = "cd-sns-lambda"
 }
 
 resource "aws_sns_topic_subscription" "lambda-subscription" {
-    topic_arn = "${aws_sns_topic.cd-sns-lambda.arn}"
-    protocol = "lambda"
-    endpoint = "${aws_lambda_function.cd_sns_lambda.arn}"
+  topic_arn = "${aws_sns_topic.cd-sns-lambda.arn}"
+  protocol  = "lambda"
+  endpoint  = "${aws_lambda_function.cd_sns_lambda.arn}"
 }
 
 data "archive_file" "slack_notification_zip" {
-  type = "zip"
+  type        = "zip"
   output_path = "${path.module}/lambda-slack.zip"
 
   source_dir = "${path.module}/functions/"
 }
 
 locals {
-  # Solution from this comment to open issue on non-relative paths
-	# https://github.com/hashicorp/terraform/issues/8204#issuecomment-332239294
+  # Solution from this comment to open issue on non-relative paths  # https://github.com/hashicorp/terraform/issues/8204#issuecomment-332239294
 
-	filename = "${substr(data.archive_file.slack_notification_zip.output_path, length(path.cwd) + 1, -1)}"
+  filename = "${substr(data.archive_file.slack_notification_zip.output_path, length(path.cwd) + 1, -1)}"
+
   // +1 for removing the "/"
 }
 
 resource "aws_lambda_function" "cd_sns_lambda" {
-  function_name     = "cd_sns_lambda"
-  role              = "${aws_iam_role.iam_for_lambda.arn}"
-  handler           = "lambda-slack.lambda_handler"
+  function_name = "cd_sns_lambda"
+  role          = "${aws_iam_role.iam_for_lambda.arn}"
+  handler       = "lambda-slack.lambda_handler"
 
-  filename          = "${local.filename}"
-  source_code_hash  = "${base64sha256(file("${local.filename}"))}"
+  filename         = "${local.filename}"
+  source_code_hash = "${base64sha256(file("${local.filename}"))}"
 
-  runtime           = "python2.7"
-  timeout           = "120"
-  kms_key_arn       = "${var.kms_key_arn}"
+  runtime     = "python2.7"
+  timeout     = "120"
+  kms_key_arn = "${var.kms_key_arn}"
+
   environment {
     variables = {
       SLACK_WEBHOOK = "${var.slack_webhook_url}"
       SLACK_CHANNEL = "${var.slack_channel}"
-      NOTIFY_USERS = "${var.notify_users}"
-      VERBOSE = "${var.verbose}"
+      NOTIFY_USERS  = "${var.notify_users}"
+      VERBOSE       = "${var.verbose}"
     }
   }
 }
 
 resource "aws_lambda_permission" "cd_sns_lambda" {
-  statement_id   = "AllowExecutionFromSNS"
-  action         = "lambda:InvokeFunction"
-  function_name  = "${aws_lambda_function.cd_sns_lambda.function_name}"
-  principal      = "sns.amazonaws.com"
-  source_arn     = "${aws_sns_topic.cd-sns-lambda.arn}"
+  statement_id  = "AllowExecutionFromSNS"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.cd_sns_lambda.function_name}"
+  principal     = "sns.amazonaws.com"
+  source_arn    = "${aws_sns_topic.cd-sns-lambda.arn}"
 }
--- notify-slack/variables.tf
+++ notify-slack/variables.tf
@@ -1,23 +1,23 @@
 variable "slack_webhook_url" {
-	description = "Needs to be encrypted from a file with _no_ encryption context, using: aws kms encrypt --key-id 'arn:' --plaintext 'fileb://webhook' --output text --query CiphertextBlob"
+  description = "Needs to be encrypted from a file with _no_ encryption context, using: aws kms encrypt --key-id 'arn:' --plaintext 'fileb://webhook' --output text --query CiphertextBlob"
 }
 
 variable "slack_channel" {
-	description = "E.g. #channel_name"
+  description = "E.g. #channel_name"
 }
 
 variable "kms_key_arn" {
-	description = "KMS used for encrypting the webhook"
+  description = "KMS used for encrypting the webhook"
 }
 
 variable "notify_users" {
-	description = "Slack usernames for mentions as a space separated string as '@name1 @name2'"
-	type = "string"
-	default = ""
+  description = "Slack usernames for mentions as a space separated string as '@name1 @name2'"
+  type        = "string"
+  default     = ""
 }
 
 variable "verbose" {
-	description = "All codedeploy messages will be output if true. Only CREATED, FAILED, STOPPED and SUCCEEDED if it is empty or false"
-	type = "string"
-	default = "true"
+  description = "All codedeploy messages will be output if true. Only CREATED, FAILED, STOPPED and SUCCEEDED if it is empty or false"
+  type        = "string"
+  default     = "true"
 }
