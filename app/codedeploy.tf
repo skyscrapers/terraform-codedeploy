skyscrapers/terraform-codedeploy

commit 2c8d528f74479287b6d6a623fbdaf546638cbeb7

Move module to tf 0.12 (#16)
* upgrade code to tf 0.12

* fix readme

* fix format

app/codedeploy.tf
@@ -1,6 +1,8 @@
-data "aws_region" "current" {}
+data "aws_region" "current" {
+}
 
-data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "current" {
+}
 
 resource "aws_codedeploy_app" "app" {
   name = "${var.project}-${var.name}"
@@ -55,7 +57,7 @@ resource "aws_iam_policy" "deployer_policy" {
       ]
     }
     ${element(formatlist(", { \"Effect\" : \"Allow\", \"Action\" : [ \"s3:PutObject*\", \"s3:ListBucket\" ], \"Resource\" : [ \"%s/*\", \"%s\" ] }, { \"Effect\" : \"Allow\", \"Action\" : [ \"s3:ListAllMyBuckets\" ], \"Resource\" : [ \"*\" ] }", compact(list(var.s3_bucket_arn)), compact(list(var.s3_bucket_arn))), 0)}
-  ]
 }
 EOF
+
 }

app/outputs.tf
@@ -1,15 +1,16 @@
 output "app_name" {
-  value = "${aws_codedeploy_app.app.name}"
+  value = aws_codedeploy_app.app.name
 }
 
 output "deployer_policy_id" {
-  value = "${aws_iam_policy.deployer_policy.id}"
+  value = aws_iam_policy.deployer_policy.id
 }
 
 output "deployer_policy_arn" {
-  value = "${aws_iam_policy.deployer_policy.arn}"
+  value = aws_iam_policy.deployer_policy.arn
 }
 
 output "deployer_policy_name" {
-  value = "${aws_iam_policy.deployer_policy.name}"
+  value = aws_iam_policy.deployer_policy.name
 }
+

app/variables.tf
@@ -10,3 +10,4 @@ variable "s3_bucket_arn" {
   description = "ARN of the S3 bucket where to fetch the application revision packages"
   default     = ""
 }
+

app/versions.tf
@@ -1,0 +1,4 @@
+
+terraform {
+  required_version = ">= 0.12"
+}

deployment-group-blue-green/main.tf
@@ -1,22 +1,22 @@
 resource "aws_codedeploy_deployment_group" "deployment_group" {
-  app_name               = "${var.app_name}"
+  app_name               = var.app_name
   deployment_group_name  = "${var.app_name}-${var.environment}"
   deployment_config_name = "CodeDeployDefault.AllAtOnce"
-  service_role_arn       = "${var.service_role_arn}"
-  autoscaling_groups     = ["${var.autoscaling_groups}"]
+  service_role_arn       = var.service_role_arn
+  autoscaling_groups     = var.autoscaling_groups
 
   auto_rollback_configuration {
-    enabled = "${var.rollback_enabled}"
-    events  = "${var.rollback_events}"
+    enabled = var.rollback_enabled
+    events  = var.rollback_events
   }
 
   blue_green_deployment_config {
     deployment_ready_option {
-      action_on_timeout = "${var.action_on_timeout}"
+      action_on_timeout = var.action_on_timeout
     }
 
     terminate_blue_instances_on_deployment_success {
-      action = "${var.terminate_blue_instances_on_deployment_success}"
+      action = var.terminate_blue_instances_on_deployment_success
     }
   }
 
@@ -26,8 +26,9 @@ resource "aws_codedeploy_deployment_group" "deployment_group" {
   }
 
   trigger_configuration {
-    trigger_events     = "${var.trigger_events}"
+    trigger_events     = var.trigger_events
     trigger_name       = "${var.app_name}-${var.environment}"
-    trigger_target_arn = "${var.trigger_target_arn}"
+    trigger_target_arn = var.trigger_target_arn
   }
 }
+

deployment-group-blue-green/variables.tf
@@ -11,7 +11,7 @@ variable "service_role_arn" {
 }
 
 variable "autoscaling_groups" {
-  type        = "list"
+  type        = list(string)
   description = "Autoscaling groups you want to attach to the deployment group"
 }
 
@@ -22,13 +22,13 @@ variable "rollback_enabled" {
 
 variable "rollback_events" {
   description = "The event types that trigger a rollback"
-  type        = "list"
+  type        = list(string)
   default     = ["DEPLOYMENT_FAILURE"]
 }
 
 variable "trigger_events" {
   description = "events that can trigger the notifications"
-  type        = "list"
+  type        = list(string)
   default     = ["DeploymentStop", "DeploymentRollback", "DeploymentSuccess", "DeploymentFailure", "DeploymentStart"]
 }
 
@@ -38,10 +38,11 @@ variable "trigger_target_arn" {
 
 variable "action_on_timeout" {
   description = "When to reroute traffic from an original environment to a replacement environment in a blue/green deployment"
-  default = "CONTINUE_DEPLOYMENT"
+  default     = "CONTINUE_DEPLOYMENT"
 }
 
 variable "terminate_blue_instances_on_deployment_success" {
   description = " The action to take on instances in the original environment after a successful blue/green deployment"
-  default = "KEEP_ALIVE"
+  default     = "KEEP_ALIVE"
 }
+

deployment-group-blue-green/versions.tf
@@ -1,0 +1,4 @@
+
+terraform {
+  required_version = ">= 0.12"
+}

deployment-group-ec2tag-notify/main.tf
@@ -1,22 +1,23 @@
 resource "aws_codedeploy_deployment_group" "deployment_group" {
-  app_name              = "${var.app_name}"
+  app_name              = var.app_name
   deployment_group_name = "${var.app_name}-${var.environment}"
-  service_role_arn      = "${var.service_role_arn}"
+  service_role_arn      = var.service_role_arn
 
   auto_rollback_configuration {
-    enabled = "${var.rollback_enabled}"
-    events  = "${var.rollback_events}"
+    enabled = var.rollback_enabled
+    events  = var.rollback_events
   }
 
   trigger_configuration {
-      trigger_events = "${var.trigger_events}"
-      trigger_name = "${var.trigger_name}"
-      trigger_target_arn = "${var.trigger_arn}"
+    trigger_events     = var.trigger_events
+    trigger_name       = var.trigger_name
+    trigger_target_arn = var.trigger_arn
   }
 
   ec2_tag_filter {
-    key   = "${var.filterkey}"
+    key   = var.filterkey
     type  = "KEY_AND_VALUE"
-    value = "${var.filtervalue}"
+    value = var.filtervalue
   }
 }
+

commit 689d83b8984a6df588c8050d86ea55bd13d08ea8

Fixing a syntax error introduced in #16

app/codedeploy.tf
@@ -57,6 +57,7 @@ resource "aws_iam_policy" "deployer_policy" {
       ]
     }
     ${element(formatlist(", { \"Effect\" : \"Allow\", \"Action\" : [ \"s3:PutObject*\", \"s3:ListBucket\" ], \"Resource\" : [ \"%s/*\", \"%s\" ] }, { \"Effect\" : \"Allow\", \"Action\" : [ \"s3:ListAllMyBuckets\" ], \"Resource\" : [ \"*\" ] }", compact(list(var.s3_bucket_arn)), compact(list(var.s3_bucket_arn))), 0)}
+   ]
 }
 EOF
 